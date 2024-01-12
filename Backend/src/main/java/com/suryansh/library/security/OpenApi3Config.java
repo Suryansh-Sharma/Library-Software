0dd9b072c4e20fbfe27f230a43f4401bf8a0df77
Suryansh Sharma <80879578+Suryansh-Sharma@users.noreply.github.com>
2024-01-12 18:20:54 +0530

    Added OpenApi Config

---
+++ Backend/src/main/java/com/suryansh/library/security/OpenApi3Config.java
@@ -0,0 +1,18 @@
+package com.suryansh.library.security;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Library Software Api", version = "v1"))
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class OpenApi3Config {
+}
